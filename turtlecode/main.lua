512b3e72c5d5363bc2248add2d86c1b079fa6dba
Filamentous <h.tourond@gmail.com>
2024-04-28 21:41:09 -0400

    relative position

--- turtlecode/main.lua
+++ turtlecode/main.lua
@@ -1,53 +1,21 @@
 local tellerMachineID = 6
 
--- GPS navigation function
-function goTo(x, y, z)
-    local curX, curY, curZ = gps.locate(5)
-    if not curX then
-        print("GPS not available")
-        return false
+-- Function to move turtle a set number of steps
+function move(steps, action)
+    for i = 1, steps do
+        action()
     end
-
-    -- Adjust height
-    while curY < y do
-        turtle.up()
-        curY = curY + 1
-    end
-    while curY > y do
-        turtle.down()
-        curY = curY - 1
-    end
-
-    -- Adjust longitude
-    while curX < x do
-        turtle.forward()
-        curX = curX + 1
-    end
-    while curX > x do
-        turtle.back()
-        curX = curX - 1
-    end
-
-    -- Adjust latitude
-    while curZ < z do
-        turtle.forward()
-        curZ = curZ + 1
-    end
-    while curZ > z do
-        turtle.back()
-        curZ = curZ - 1
-    end
-    return true
 end
 
 -- Function to collect items from a chest
 function collectItems()
     turtle.select(1)  -- Select the first slot
     while turtle.suck() do
-        -- Collect items until the chest is empty
+        -- Keep collecting items until the chest is empty
     end
 end
 
+-- Function to send item data to the teller machine
 function sendItemData()
     local items = {}
     for slot = 1, 16 do
@@ -70,24 +38,27 @@ function depositItems()
     end
 end
 
--- Main routine modification to wait for activation command
+-- Navigation to predefined locations and actions
+function performSequence()
+    -- Starting at initial position, assume it starts at 223, 67, -395 (player input chest location)
+    collectItems()  -- Collect from player input chest
+    move(1, turtle.back)  -- Move back to 223, 67, -397
+    move(3, turtle.up)  -- Move up to 223, 70, -397
+    move(9, turtle.back)  -- Move back to 223, 70, -406 (deposit location)
+    sendItemData()  -- Send collected items data
+    depositItems()  -- Deposit items into the chest below
+    move(9, turtle.forward)  -- Move forward to 223, 70, -397
+    move(3, turtle.down)  -- Move down to 223, 67, -397
+    move(1, turtle.forward)  -- Return to starting position at 223, 67, -395
+end
+
+-- Main routine waiting for activation command
 function main()
     rednet.open("right")  -- Ensure the modem is open
     while true do
         local senderId, message, protocol = rednet.receive("turtleCommand")
         if protocol == "turtleCommand" and message.command == "activate" and senderId == tellerMachineID then
-            -- Navigate to deposit chest
-            if goTo(223, 67, -395) then
-                collectItems()
-                goTo(223, 67, -397)
-                goTo(223, 64, -397)
-                goTo(223, 64, -406)  
-                sendItemData()
-                depositItems()
-                goTo(223, 64, -406)
-                goTo(223, 64, -397)
-                goTo(223, 67, -397)
-            end
+            performSequence()
         end
     end
 end
